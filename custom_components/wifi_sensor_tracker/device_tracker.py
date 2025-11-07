5efe4d32540dfa6563daffe92be6a8705c5a6f54
5a2v0 <saving@hotmail.it>
2025-11-07 20:43:48 +0100

    clean up code
    clean up code and add fallback strategy to hide gps_accuracy attribute when core patch is not applicable

--- custom_components/wifi_sensor_tracker/device_tracker.py
+++ custom_components/wifi_sensor_tracker/device_tracker.py
@@ -1,13 +1,15 @@
-"""Device tracker per Wi-Fi Sensor Tracker (multi-zona, con consider_home)."""
+
+"""Device tracker per Wi-Fi Sensor Tracker (multi-zona)."""
 import logging
 from datetime import timedelta
 from homeassistant.components.device_tracker import SourceType
 from homeassistant.components.device_tracker.config_entry import TrackerEntity
-from homeassistant.const import STATE_UNAVAILABLE
+from homeassistant.const import STATE_UNAVAILABLE, STATE_HOME, STATE_NOT_HOME, ATTR_FRIENDLY_NAME
+from homeassistant.components.zone import ENTITY_ID_HOME
 from homeassistant.core import callback
 from homeassistant.helpers.event import async_track_state_change_event, async_call_later
 from homeassistant.util import dt as dt_util
-from homeassistant.const import ATTR_FRIENDLY_NAME
+from .patch_person import WORKAROUND_HIDE_GPS_ACCURACY
 
 _LOGGER = logging.getLogger(__package__)
 
@@ -19,7 +21,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
     consider_home = entry.data.get("consider_home", 180)
     extra_zones = entry.data.get("extra_zones", [])
     
-    # Mappa SSID → Nome zona
+    # Mappa SSID → entity_id della zona come memorizzato nel config entry
     ssid_zone_map = {z["ssid"]: z["zone"] for z in extra_zones if "ssid" in z and "zone" in z}
 
     entities = [
@@ -42,10 +44,11 @@ class WifiSensorTrackerEntity(TrackerEntity):
         self._attr_is_connected = False
         self._attr_latitude = None
         self._attr_longitude = None
-        # Inizializziamo gps_accuracy a None per evitare che il core mostri 0
-        #self._attr_gps_accuracy = None
+        # Se la patch del core non è stata applicata inizializziamo gps_accuracy a None per evitare che il core mostri questo attributo con valore 0
+        if WORKAROUND_HIDE_GPS_ACCURACY:
+            self._attr_gps_accuracy = None
         self._ssid_zone_map = ssid_zone_map or {}
-        self._current_zone = "not_home"
+        self._current_zone = STATE_NOT_HOME
         self._consider_home = timedelta(seconds=consider_home)
         self._remove_listener = None
         self._exit_timer = None  # inizializza il timer
@@ -56,7 +59,7 @@ class WifiSensorTrackerEntity(TrackerEntity):
 
     @property
     def state(self):
-        return self._current_zone if self._attr_is_connected else "not_home"
+        return self._current_zone if self._attr_is_connected else STATE_NOT_HOME
 
     @property
     def extra_state_attributes(self):
@@ -65,8 +68,9 @@ class WifiSensorTrackerEntity(TrackerEntity):
         if self._attr_latitude is not None and self._attr_longitude is not None:
             attrs["latitude"] = self._attr_latitude
             attrs["longitude"] = self._attr_longitude
-        # Forzo l'attributo None che diventerà null in Json e non verrà mostrato nella UI
-        #attrs["gps_accuracy"] = self._attr_gps_accuracy
+        # Se la patch del core non è stata applicata forzo l'attributo a 'None' che diventerà 'null' in Json e non verrà mostrato nella UI
+        if WORKAROUND_HIDE_GPS_ACCURACY:
+            attrs["gps_accuracy"] = self._attr_gps_accuracy
         return attrs
 
     def _schedule_exit(self):
@@ -116,16 +120,16 @@ class WifiSensorTrackerEntity(TrackerEntity):
         if state.state == self._ssid_home or state.state in self._ssid_zone_map:
             self._attr_is_connected = True
             if state.state == self._ssid_home:
-                self._current_zone = "home"
-                # Azzeriamo latitude e longitude quando siamo "home"
+                self._current_zone = STATE_HOME
+                # Azzeriamo latitude e longitude quando siamo "home", il core li prenderà automaticamente dalla zona
                 self._attr_latitude = None
                 self._attr_longitude = None
             else:
                 zone_entity_id = self._ssid_zone_map[state.state]
                 # se la zona extra è "zone.home", trattala come home
-                if zone_entity_id == "zone.home":
-                    self._current_zone = "home"
-                    # Azzeriamo latitude e longitude quando siamo "home"
+                if zone_entity_id == ENTITY_ID_HOME:
+                    self._current_zone = STATE_HOME
+                    # Azzeriamo latitude e longitude quando siamo "home", il core li prenderà automaticamente dalla zona
                     self._attr_latitude = None
                     self._attr_longitude = None
                 else:
@@ -133,25 +137,28 @@ class WifiSensorTrackerEntity(TrackerEntity):
                     if zone_state:
                         # friendly name (es. "Lavoro", "Scuola", ecc.)
                         self._current_zone = zone_state.attributes.get(
-                            ATTR_FRIENDLY_NAME, #"friendly_name",
-                            zone_entity_id.partition("zone.")[2]
+                            ATTR_FRIENDLY_NAME,
+                            zone_entity_id.partition("zone.")[2] # fallback --> se la zona non avesse un friendly name lo creo
                         )
                         # Aggiorniamo latitude e longitude se la zona è trovata
                         self._attr_latitude = zone_state.attributes.get("latitude")
                         self._attr_longitude = zone_state.attributes.get("longitude")
                     else:
-                        # fallback: togli "zone." se presente
-                        self._current_zone = zone_entity_id.partition("zone.")[2] or zone_entity_id
-                        # Se la zona non esiste, azzeriamo latitude e longitude
+                        # fallback: zona non esistente (esempio: zona cancellata ma rimasta nelle opzioni dell'integrazione). Tolgo "zone." e creo un friendly name
+                        self._current_zone = zone_entity_id.partition("zone.")[2].capitalize() or zone_entity_id
+                        _LOGGER.warning("Zona %s non trovata in HA, usando fallback '%s'", zone_entity_id, self._current_zone)
+                        # La zona non esiste, azzeriamo latitude e longitude
                         self._attr_latitude = None
                         self._attr_longitude = None
-                        
+
             self.async_write_ha_state()
-            
+
             # se c’era un timer di uscita → annullalo
             if self._exit_timer:
                 self._exit_timer()
                 self._exit_timer = None
+
+        # Se non risultiamo in nessuna zona esistente programmiamo il not_home dopo consider_home
         else:
             self._schedule_exit()
 
